9615c7998b1f7bc0e1eb4f353057df12e053d062
Alfons Pastor <alfons.pastor.andreu@gmail.com>
2025-06-15 15:55:40 +0200

    Abstracción IStopwatchService
    Define una interfaz para el comportamiento y el estado de un cronómetro

--- StopwatchApp.Core/IStopwatchService.cs
+++ StopwatchApp.Core/IStopwatchService.cs
@@ -1,7 +1,15 @@
 namespace StopwatchApp.Core
 {
+    public enum StopwatchState { Stopped, Running, Paused }
     public interface IStopwatchService
     {
-    }
+        TimeSpan Elapsed { get; }
+        StopwatchState State { get; }
 
+        void Start();
+        void Pause();
+        void Stop();
+        event EventHandler<TimeSpan>? ElapsedChanged;
+
+    }
 }
